8ff91f115b158fa012b3a5d34f92c73e22822475
Raghvendra Singh <raghv.afgji@icloud.com>
2025-02-12 16:17:16 +0530

    Initial outlining done

---
+++ game_data.py
@@ -0,0 +1,20 @@
+person_list = [
+    {
+        'name': 'Instagram',
+        'follower_count': 500,
+        'description': 'Social-Media Application',
+        'country': 'American',
+    },
+    {
+        'name': 'Raghvendra',
+        'follower_count': 50,
+        'description': 'Application Developer',
+        'country': 'Indian',
+    },
+    {
+        'name': 'Ankit',
+        'follower_count': 30,
+        'description': 'Quality Analyst Intern',
+        'country': 'Indian',
+    },
+]
